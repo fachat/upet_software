e6d7b9a599d508e7951b0aeb07b89a8d8c2bd406
Andre Fachat <afachat@gmx.de>
2025-04-20 17:52:03 +0200

    initial version of flashmon (no write)

---
+++ utilities/spi-UPET.s
@@ -0,0 +1,131 @@
+;-----------------------------------------------------------------------------
+; spi.s
+; Copyright (C) 2023 Andre Fachat
+;
+; Using the interface described here
+; https://github.com/fachat/MicroPET/blob/main/CPLD/SPI.md
+;
+; Changes to SD-Card version: 
+; - sector size is 256 byte
+; - error flag is C=1
+; - inactive is device 7 selected
+;-----------------------------------------------------------------------------
+
+	.include "spi.inc"
+
+;	.import sector_buffer
+
+;	.export spi_ctrl, spi_read, spi_write, spi_select, spi_deselect, spi_read_sector, spi_write_sector
+
+
+
+;-----------------------------------------------------------------------------
+; Registers
+;-----------------------------------------------------------------------------
+SPI_CTRL      = $e808
+SPI_DATA      = $e809
+SPI_PEEK      = $e80a
+
+SPI_CTRL_SELECT_MASK	= %00000111
+
+;-----------------------------------------------------------------------------
+; deselect card
+;
+; clobbers: A
+;-----------------------------------------------------------------------------
+spi_deselect:
+	lda SPI_CTRL
+	ora #SPI_CTRL_SELECT_MASK
+	sta SPI_CTRL
+	rts
+
+;-----------------------------------------------------------------------------
+; select card
+;
+; clobbers: A,X,Y
+;-----------------------------------------------------------------------------
+spi_select:
+	pha
+	lda SPI_CTRL
+	ora #SPI_CTRL_SELECT_MASK
+	sta SPI_CTRL
+	pla
+	ora #255-SPI_CTRL_SELECT_MASK
+	and SPI_CTRL
+	sta SPI_CTRL
+	rts
+
+
+;-----------------------------------------------------------------------------
+; spi_read
+;
+; result in A
+;-----------------------------------------------------------------------------
+spi_read:
+	lda #$FF	; 2
+spi_rw:
+	sta SPI_DATA	; 4
+@1:	bit SPI_CTRL	; 4
+	bmi @1		; 2 + 1 if branch
+	lda SPI_PEEK	; 4
+	rts		; 6
+			; >= 22 cycles
+
+;-----------------------------------------------------------------------------
+; spi_write
+;
+; byte to write in A
+;-----------------------------------------------------------------------------
+spi_write:
+	sta SPI_DATA
+@1:	bit SPI_CTRL
+	bmi @1
+	rts
+
+;-----------------------------------------------------------------------------
+; sdcard_init
+; result: C=0 -> error, C=1 -> success
+;-----------------------------------------------------------------------------
+spi_ctrl:
+	ora #SPI_CTRL_SELECT_MASK
+	sta SPI_CTRL
+	rts
+
+
+;-----------------------------------------------------------------------------
+; spi_read_sector
+; read 256 bytes from SPI to sector_buffer
+; result: C=1 -> error, C=0 -> success
+;-----------------------------------------------------------------------------
+spi_read_sector:
+
+	; Read 256 bytes of sector data
+        ldy #0
+
+@1:	jsr spi_read
+	sta sector_buffer + 0, y
+	iny
+	bne @1
+	clc
+	rts
+
+;-----------------------------------------------------------------------------
+; spi_write_sector
+; write 256 bytes of data from sector_buffer
+; result: C=1 -> error, C=0 -> success
+;-----------------------------------------------------------------------------
+spi_write_sector:
+
+        ; Send 512 bytes of sector data
+        ldy #0
+
+@1:     lda sector_buffer, y            ; 4
+        sta SPI_DATA
+@3:	bit SPI_CTRL
+	bmi @3
+        iny                             ; 2
+        bne @1                          ; 2 + 1
+
+	clc
+        rts
+
